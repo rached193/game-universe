c9d86513bf0062feae5210f1a15d8eb3b78c47ef
Rached <archer193@hotmail.com>
2019-03-09 00:12:56 +0100

    Cambiar ruta de archivos estaticos

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/game-universe'));
+app.use(express.static(__dirname + '/dist/game-universe'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/game-universe/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/game-universe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
