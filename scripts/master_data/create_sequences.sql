52ea878973631d73011bda0654acaeb3c52e0ade
Jorge Campo Oliván <perdiganogaming@gmail.com>
2019-04-08 19:05:03 +0200

    scripts

---
+++ scripts/master_data/create_sequences.sql
@@ -0,0 +1,17 @@
+/*Videogame*/
+CREATE SEQUENCE master_data.VIDEOGAME_SEQ;
+
+/*Platform*/
+CREATE SEQUENCE master_data.PLATFORM_SEQ;
+
+/*Region*/
+CREATE SEQUENCE master_data.REGION_SEQ;
+
+/*GameMode*/
+CREATE SEQUENCE master_data.GAMEMODE_SEQ;
+
+/*Format*/
+CREATE SEQUENCE master_data.FORMAT_SEQ;
+
+/*BO*/
+CREATE SEQUENCE master_data.BO_SEQ;
